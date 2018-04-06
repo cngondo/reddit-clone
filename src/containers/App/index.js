9a37fb35d549f45b6067ad17ed2049b44ecd3316
cngondo <devngondo@gmail.com>
2018-04-06 16:25:20 -0500

    Added firebase to project

--- src/containers/App/index.js
+++ src/containers/App/index.js
@@ -1,6 +1,31 @@
 import React, { Component } from 'react';
+import * as firebase from 'firebase';
+import config from './config/firebase-config';
 
 class App extends Component {
+	constructor(){
+		super();
+		// this.state = {
+		// 	posts: snapshot.val(),
+		// 	loading: false
+		// }
+		//initialize firebase
+		firebase.initializeApp(config);		 
+	}
+
+	componentWillMount(){
+		let postsRef = firebase.database().ref('posts');
+
+		postsRef.on('value', (snapshot)=>{
+			console.dir(snapshot.val());
+
+			this.setState({
+				posts: snapshot.val(),
+				loading: false
+			});
+		});
+	}
+
 	render(){
 		return (
 			<div className="App">
